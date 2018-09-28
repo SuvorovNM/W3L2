47d8d86eaeb6bfc97718cfdb75f4f0ae3023c52d
SuvorovNM <35630837+SuvorovNM@users.noreply.github.com>
2018-09-28 17:48:55 +0500

    Добавьте файлы проекта.

---
+++ W3L2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace W3L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader std = new StreamReader("input.txt");
+            StreamWriter stw = new StreamWriter("output.txt");
+            string str = std.ReadLine();
+            string[] info = str.Split(' ');
+            int n = Int32.Parse(info[0]);
+            int m = Int32.Parse(info[1]);
+            int k = Int32.Parse(info[2]);
+            char[,] arr = new char[n,m];
+            int[] pos1 = new int[n];
+            int[] pos2 = new int[n];
+            //Считывание символов массива
+            for (int i = 0; i < n; i++)
+            {
+                str = std.ReadLine();
+                //info = str.Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = str[j];
+                }
+                pos1[i] = i + 1;//poss_pred
+                pos2[i] = i + 1;//poss_after                
+            }
+            char[] temparray = new char[m];
+            int min_idx;
+            for (int i = 1; i <= k; i++)//Сортировка "построчно" начиная с конца
+            {
+                for (int p = 0; p < m; p++)
+                {
+                    temparray[p] = arr[n - i, pos1[p]-1];
+                }
+                for (int t = 0; t < m - 1; t++)//Select-sort
+                {
+                    min_idx = t;
+                    for (int j = t + 1; j < m; j++)
+                    {
+                        if (arr[n - i, j] < arr[n - i, min_idx])
+                            min_idx = j;
+                    }
+                        char temp = arr[n-i, t];
+                        arr[n - i, t] = arr[n-i,min_idx];
+                        arr[n - i, min_idx] = temp;
+                        int tmpIndex = pos1[t];
+                        pos1[t] = pos1[min_idx];
+                        pos1[min_idx] = tmpIndex;
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                stw.Write(pos1[i] + " ");
+            }
+            stw.Close();
+        }
+    }
+}
---
+++ W3L2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("W3L2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("W3L2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("45200dfe-f232-4efd-ab20-474183a37945")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
